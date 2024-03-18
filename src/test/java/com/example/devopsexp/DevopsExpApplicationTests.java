d98250f493095e209a75d0d1f015ed42ed92b272
ImArnav19 <morearnav019@gmail.com>
2024-03-18 23:55:23 +0530

    Initial commit

---
+++ src/test/java/com/example/devopsexp/DevopsExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
